984601e66d895223541371901f68a71bcd1c3a21
Greg Brunk <greg.brunk@gmail.com>
2016-07-26 08:54:10 -0600

    got down to 5 errors :)

--- wheel_of_fortune.rb
+++ wheel_of_fortune.rb
@@ -1,18 +1,28 @@
 class WheelOfFortune
-  def initialize
-    p "Happy coding!"
+  attr_reader :theme, :phrase 
+  attr_accessor :guesses
+  def initialize(hash)
+    @theme = hash[:theme]
+    @phrase = hash[:phrase]
+    @guesses = []
   end
 
   def to_s
-    nil
+    return @phrase
   end
 
   def can_i_have?(input)
-    nil
+    downcase_input = input.downcase
+    @guesses.push(downcase_input)
+    downcase_phrase_array = @phrase.downcase.chars
+    if downcase_phrase_array.include? downcase_input
+      return true
+    else 
+      return false
+    end
   end
 
   def game_over?
-    nil
   end
 end
 
